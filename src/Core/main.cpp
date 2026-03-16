dfc17571c21957a84027f10419155ef29ccfeef1
janumaruku <jean-marc.kottin@ade-labs.com>
2026-03-16 15:59:34 +0100

    add: add building files

---
+++ src/Core/main.cpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2026
+** Arcade
+** File description:
+** main
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
