428c12af429493731afe8a07a8c496dd98109ad7
janumaruku <jean-marc.kottin@epitech.eu>
2026-04-01 15:48:40 +0200

    chore: ACP-11 #comment Add core in main for test

--- src/Core/main.cpp
+++ src/Core/main.cpp
@@ -7,9 +7,17 @@
 
 #include <iostream>
 
+#include "Core.hpp"
+
 int main()
 {
     std::cout << "Hello World!" << std::endl;
+
+    try {
+        arcade::core::Core core{"./libs/arcade_ncurses.so"};
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
 
     return 0;
 }
