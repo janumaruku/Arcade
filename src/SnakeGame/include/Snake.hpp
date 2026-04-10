a1301a95f88aa99fc34c8e0bc4e73880d0f2060c
Nicky Truong <nicky.truong@epitech.eu>
2026-04-10 18:01:51 +0200

    feat: Add interface, struct, enum and all prerequisites for the Snake Game

--- src/SnakeGame/include/Snake.hpp
+++ src/SnakeGame/include/Snake.hpp
@@ -8,30 +8,60 @@
 #ifndef SNAKE_HPP_
 #define SNAKE_HPP_
 #include <iostream>
-#include <vector>
-#include <deque>
 #include <chrono>
 #include "GameInterface.hpp"
+#include "Widget.hpp"
+
+#define GRID_SIZE_X 45
+#define GRID_SIZE_Y 30
 
 namespace arcade {
 namespace game {
-class Snake : public IGameModule{
+namespace snake {
+typedef enum {
+    UP,
+    LEFT,
+    RIGHT,
+    DOWN
+} Direction;
+
+typedef struct {
+    widget::Vec2 head_pos;
+    std::vector<widget::Vec2> pos;
+    snake::Direction dir;
+    long long score;
+    bool addTail;
+} Player;
+} // snake
+
+static const std::unordered_map<snake::Direction, std::string> SnakeUTF8 = {
+    {snake::RIGHT, "▶"},
+    {snake::LEFT,  "◀"},
+    {snake::UP,    "▲"},
+    {snake::DOWN,  "▼"},
+};
+
+class SnakeGame : public IGameModule {
 public:
-    Snake() noexcept;
-    ~Snake();
-    void gameLogic();
-    void move(std::vector<std::pair<int, int>> body);
-    void getApplePosition(int AppleX, int AppleY);
-    std::vector<std::pair<int, int>> body;
-    std::vector<std::pair<int, int>> applePos;
+    SnakeGame();
+    ~SnakeGame() override = default;
+
+    [[nodiscard]] std::string getName() const noexcept override;
+    void start() noexcept override;
+    [[nodiscard]] const widget::GameState &getGameState() noexcept override;
+    void userEvent(const widget::Event &input) override;
+    [[nodiscard]] const widget::Resource &getResource() const noexcept override;
+
 protected:
 private:
-    int _snakeX;
-    int _snakeY;
-    int _appleX;
-    int _appleY;
-    int _width;
-    int _length;
+    snake::Player _snakebody;
+    snake::Direction _direction;
+    std::vector<std::size_t> _map;
+    void updateDirection();
+    void updateGrid();
+    void addFood();
+    widget::GameState _gamest;
+    bool _gameover;
 };
 } // game
 } // arcade
